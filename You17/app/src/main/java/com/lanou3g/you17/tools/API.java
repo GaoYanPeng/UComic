049b4b3b11eb3c5d2dba4da02ee51b7eac4d0ab2
高鹏 <1598039446@qq.com>
2016-09-03 15:01:05 +0800

    删除一些东西

--- You17/app/src/main/java/com/lanou3g/you17/tools/API.java
+++ You17/app/src/main/java/com/lanou3g/you17/tools/API.java
@@ -46,7 +46,5 @@ public class API {
      */
     public static final String MOVED ="http://app.u17.com/v3/appV3/android/phone/list/commonComicList?argValue=15&argName=sort&page=1&android_id=3d6f8f613d878af1&v=3110099&model=Google+Nexus+5+-+4.4.4+-+API+19+-+1080x1920&come_from=shouji360";
 
-    //首页轮播图接口
-//    public static final String API_SHUFFLING_FIGURE="http://app.u17.com/v3/appV3/android/phone/comic/boutiqueListNew?%22%20+%20%20%20%20%20%20%20%20%20%20%20%20%22android_id=3d6f8f613d878af1&v=3110099&model=Google+Nexus+5+-+4.4.4+-+API+19+-+1080x1920&come_%22%20+%20%20%20%20%20%20%20%20%20%20%20%20%22from=shouji360&key=6d785a6a482c2cc66517a5d09013c229dbd145e4f4b34e8ebc04b0c04a4836f8781a2df660e4%22%20+%20%20%20%20%20%20%20%20%20%20%20%20%22d74643aa584acfe831971b56c7e25ce7f16e1e51974feadd1da813bd70d86590efbaf0deacbdd0a4824ee179ae3fbbc0%22%20+%20%20%20%20%20%20%20%20%20%20%20%20%2289913559b6cf6e4da37587b212717e08ed56%253A%253A%253Au17";
 
 }
